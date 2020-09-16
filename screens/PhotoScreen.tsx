44334fc6d21f51bc6c8a2d86bd92655f32029d9b
Simoncar <simoncar@gmail.com>
2020-09-16 13:16:32 -0700

    setting up for photo save

--- screens/PhotoScreen.tsx
+++ screens/PhotoScreen.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, TouchableHighlight, Dimensions } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { Camera } from 'expo-camera';
-import { Snap } from "../util/photo"
+import { Entypo } from "@expo/vector-icons";
+
+import { savePhoto } from "../util/photo"
+const WINDOW_WIDTH = Dimensions.get("window").width;
 
 export default function PhotoScreen({ navigation }) {
 
@@ -23,9 +26,10 @@ export default function PhotoScreen({ navigation }) {
 		return <Text>No access to camera</Text>;
 	}
 
-	 snapPhoto(() => {
-
-	})
+	function doStuff(url: string): string {
+		console.log("im doing stuff")
+		return url
+	}
 
 	return (
 		<View style={{ flex: 1 }}>
@@ -36,6 +40,14 @@ export default function PhotoScreen({ navigation }) {
 						backgroundColor: 'transparent',
 						flexDirection: 'row',
 					}}>
+
+					<TouchableHighlight style={styles.camera} underlayColor="#ff7043"
+						onPress={() => { doStuff("this") }}
+					>
+						<Entypo name="camera" size={28} color={"white"} />
+					</TouchableHighlight>
+
+
 					<TouchableOpacity
 						style={{
 							flex: 0.1,
@@ -72,6 +84,27 @@ const styles = StyleSheet.create({
 		marginVertical: 30,
 		height: 1,
 		width: '80%',
+	},
+	camera: {
+		backgroundColor: "#ff5722",
+		borderColor: "#ff5722",
+		borderWidth: 1,
+		height: 80,
+		width: 80,
+		borderRadius: 80 / 2,
+		alignItems: "center",
+		justifyContent: "center",
+		position: "absolute",
+		bottom: 30,
+		left: WINDOW_WIDTH / 2 - 35,
+		shadowColor: "#000000",
+		shadowOpacity: 0.8,
+		shadowRadius: 2,
+		shadowOffset: {
+			height: 1,
+			width: 0,
+		},
+		zIndex: 1,
 	},
 
 });
